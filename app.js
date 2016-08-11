69597751253b0fcbbaee76cda6009ef1ff80e1d7
Matt Anderson <matt@mattbanderson.com>
2016-08-10 20:57:37 -0400

    Change endpoints to be slightly more RESTful. Rename garage doors from 'left' and 'right' to 'one' and 'two'. Only show single garage door button in UI.

--- app.js
+++ app.js
@@ -19,24 +19,24 @@ app.get("/api/ping", function(req, res) {
 	res.json("pong");
 });
 
-app.post("/api/garage/left", function(req, res) {
+app.post("/api/garage/door/1", function(req, res) {
 	async.series([
 		function(callback) {
 			// Open pin for output
-			gpio.open(config.LEFT_GARAGE_PIN, "output", callback);
+			gpio.open(config.GARAGE_DOOR_1_PIN, "output", callback);
 		},
 		function(callback) {
 			// Turn the relay on
-			gpio.write(config.LEFT_GARAGE_PIN, config.RELAY_ON, callback);
+			gpio.write(config.GARAGE_DOOR_1_PIN, config.RELAY_ON, callback);
 		},
 		function(callback) {
 			// Turn the relay off after delay to simulate button press
-			delayPinWrite(config.LEFT_GARAGE_PIN, config.RELAY_OFF, callback);
+			delayPinWrite(config.GARAGE_DOOR_1_PIN, config.RELAY_OFF, callback);
 		},
 		function(err, results) {
 			setTimeout(function() {
 				// Close pin from further writing
-				gpio.close(config.LEFT_GARAGE_PIN);
+				gpio.close(config.GARAGE_DOOR_1_PIN);
 				// Return json
 				res.json("ok");
 			}, config.RELAY_TIMEOUT);
@@ -44,24 +44,24 @@ app.post("/api/garage/left", function(req, res) {
 	]);
 });
 
-app.post("/api/garage/right", function(req, res) {
+app.post("/api/garage/door/2", function(req, res) {
 	async.series([
 		function(callback) {
 			// Open pin for output
-			gpio.open(config.RIGHT_GARAGE_PIN, "output", callback);
+			gpio.open(config.GARAGE_DOOR_2_PIN, "output", callback);
 		},
 		function(callback) {
 			// Turn the relay on
-			gpio.write(config.RIGHT_GARAGE_PIN, config.RELAY_ON, callback);
+			gpio.write(config.GARAGE_DOOR_2_PIN, config.RELAY_ON, callback);
 		},
 		function(callback) {
 			// Turn the relay off after delay to simulate button press
-			delayPinWrite(config.RIGHT_GARAGE_PIN, config.RELAY_OFF, callback);
+			delayPinWrite(config.GARAGE_DOOR_2_PIN, config.RELAY_OFF, callback);
 		},
 		function(err, results) {
 			setTimeout(function() {
 				// Close pin from further writing
-				gpio.close(config.RIGHT_GARAGE_PIN);
+				gpio.close(config.GARAGE_DOOR_2_PIN);
 				// Return json
 				res.json("ok");
 			}, config.RELAY_TIMEOUT);
@@ -69,37 +69,37 @@ app.post("/api/garage/right", function(req, res) {
 	]);
 });
 
-app.post("/api/garage/both", function(req, res) {
+app.post("/api/garage/all", function(req, res) {
 	async.series([
 		function(callback) {
 			// Open pin for output
-			gpio.open(config.LEFT_GARAGE_PIN, "output", callback);
+			gpio.open(config.GARAGE_DOOR_2_PIN, "output", callback);
 		},
 		function(callback) {
 			// Open pin for output
-			gpio.open(config.RIGHT_GARAGE_PIN, "output", callback);
+			gpio.open(config.GARAGE_DOOR_1_PIN, "output", callback);
 		},
 		function(callback) {
 			// Turn the relay on
-			gpio.write(config.LEFT_GARAGE_PIN, config.RELAY_ON, callback);
+			gpio.write(config.GARAGE_DOOR_2_PIN, config.RELAY_ON, callback);
 		},
 		function(callback) {
 			// Turn the relay on
-			gpio.write(config.RIGHT_GARAGE_PIN, config.RELAY_ON, callback);
+			gpio.write(config.GARAGE_DOOR_1_PIN, config.RELAY_ON, callback);
 		},
 		function(callback) {
 			// Turn the relay off after delay to simulate button press
-			delayPinWrite(config.LEFT_GARAGE_PIN, config.RELAY_OFF, callback);
+			delayPinWrite(config.GARAGE_DOOR_2_PIN, config.RELAY_OFF, callback);
 		},
 		function(callback) {
 			// Turn the relay off after delay to simulate button press
-			delayPinWrite(config.RIGHT_GARAGE_PIN, config.RELAY_OFF, callback);
+			delayPinWrite(config.GARAGE_DOOR_1_PIN, config.RELAY_OFF, callback);
 		},
 		function(err, results) {
 			setTimeout(function() {
 				// Close pin from further writing
-				gpio.close(config.LEFT_GARAGE_PIN);
-				gpio.close(config.RIGHT_GARAGE_PIN);
+				gpio.close(config.GARAGE_DOOR_1_PIN);
+				gpio.close(config.GARAGE_DOOR_2_PIN);
 				// Return json
 				res.json("ok");
 			}, config.RELAY_TIMEOUT);
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.LEFT_GARAGE_PIN = 16;
-config.RIGHT_GARAGE_PIN = 11;
+config.GARAGE_DOOR_1_PIN = 11;
+config.GARAGE_DOOR_2_PIN = 16;
 
 config.RELAY_ON = 0;
 config.RELAY_OFF = 1;
