02c22791c3799194e3a01b2d4bd1cb8b096e1b57
Matt Anderson <matt@mattbanderson.com>
2016-10-23 20:22:29 -0400

    Add CORS headers.

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.set('port', process.env.PORT || 3000);
 
 app.use('/', express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 function delayPinWrite(pin, value, callback) {
 	setTimeout(function() {
 		gpio.write(pin, value, callback);
